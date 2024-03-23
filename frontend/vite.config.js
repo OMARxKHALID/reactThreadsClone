1096133bff8ec951dd6df2bafcc28542ffd2ce8d
OMARxKHALID <mirxaumar1212@gmail.com>
2024-03-23 15:51:15 +0500

    finally deployed

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
@@ -11,6 +12,9 @@ export default defineConfig({
         changeOrigin: true,
         secure: false,
       },
+    },
+    fs: {
+      cachedChecks: false,
     },
   },
 });
